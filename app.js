f962d6606cbab487fca4c696feaf3353881d4ef4
atoraymond <kwamea236@protonmail.com>
2026-08-14 15:46:02 +0000

    website updated

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
-window.onload = function(){
-    document.body.className += " loaded";
-}
+const contactHover = document.querySelector("#contact-hover");
+
+const popup = document.querySelector("#popup");
+
+contactHover.addEventListener("mouseover", (e)=>{
+    popup.classList.toggle("md:block");
+});
